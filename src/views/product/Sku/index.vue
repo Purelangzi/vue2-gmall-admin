932fb13d28fa51ae9dc04d9aec5f752ffae31be1
langzi <1657884510@qq.com>
2022-11-21 16:56:38 +0800

    first

---
+++ src/views/product/Sku/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Sku',
+  data () {
+    return {
+        
+    }
+  },
+  methods: {
+    
+  },
+  created() {
+    
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
